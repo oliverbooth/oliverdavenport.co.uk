23540ea5ab3e3cf60bf3e1a1a5b617d98db77ef6
Oliver Davenport <me@olivr.me>
2017-01-15 06:52:40 +0000

    :bug: Fixed viewport scale
    improving visibility on mobile devices

--- src/index.php
+++ src/index.php
@@ -2,6 +2,7 @@
 <html>
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/css/tether.min.css">
