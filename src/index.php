d4fb172c5f446106252c2ac3f82ad2dd7b2a9114
Oliver Davenport <me@olivr.me>
2017-01-15 06:12:00 +0000

    :sparkles: Landing page

---
+++ src/index.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/css/tether.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+    <link rel="stylesheet" href="/css/style.min.css">
+    <title>Oliver Davenport</title>
+</head>
+
+<body>
+    <div class="d-flex align-items-center full-vh full-vw">
+        <div class="center-margin text-center">
+            <h1><strong>Oliver Davenport</strong></h1>
+            <h2>
+                <i class="fa fa-gamepad text-success" data-toggle="tooltip" data-placement="bottom" title="Game Designer"></i>
+                <i class="fa fa-code text-primary" data-toggle="tooltip" data-placement="bottom" title="Software Developer"></i>
+                <i class="fa fa-globe text-warning" data-toggle="tooltip" data-placement="bottom" title="Web Designer"></i>
+                <i class="fa fa-coffee text-danger" data-toggle="tooltip" data-placement="bottom" title="Coffee Drinker"></i>
+            </h2>
+            <hr>
+            <ul class="list-inline h2">
+                <li class="list-inline-item"><a href="https://github.com/oliverlukedavenport/" data-toggle="tooltip" data-placement="top" title="oliverlukedavenport"><i class="fa fa-github"></i></a></li>
+                <li class="list-inline-item"><a href="https://twitter.com/oldavenport/" data-toggle="tooltip" data-placement="top" title="@oldavenport"><i class="fa fa-twitter"></i></a></li>
+            </ul>
+            <small class="text-muted">&copy; <?= date("Y") ?></small>
+        </div>
+    </div>
+    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"></script>
+    <script src="/js/main.min.js"></script>
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-40675335-1', 'auto');
+        ga('send', 'pageview');
+    </script>
+</body>
+</html>
