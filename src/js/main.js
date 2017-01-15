33d4e056cd74e5ca0a3e4763f1f7d1ab14b4e940
Oliver Davenport <me@olivr.me>
2017-01-15 10:03:23 +0000

    :sparkles: Override Ctrl+U to open GitHub

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,27 @@
+var KEY_CTRL = 17;
+var KEY_CMD  = 91;
+
 $(function () {
     "use strict";
-    $('[data-toggle="tooltip"]').tooltip();
+    $("[data-toggle=\"tooltip\"]").tooltip();
+    
+    var ctrl = false;
+    $(document).keydown(function (e) {
+        if (e.keyCode === KEY_CTRL || e.keyCode === KEY_CMD) {
+            ctrl = true;
+        }
+    }).keyup(function (e) {
+        if (e.keyCode === KEY_CTRL || e.keyCode === KEY_CMD) {
+            ctrl = false;
+        }
+    });
+    
+    $("*").keydown(function (e) {
+        if (ctrl && (e.keyCode === 85 /* U */)) {
+            e.stopPropagation();
+            e.preventDefault();
+            
+            window.open("https://github.com/oliverlukedavenport/oliverdavenport.co.uk", "_blank");
+        }
+    });
 });
